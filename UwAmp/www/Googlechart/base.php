5bad1bd80df47d4e0c6c1adb326aa8d7f8b3e8e9
Pierre RUDELOU <pierre.rudelou@hotmail.fr>
2016-03-07 21:59:30 +0100

    Installation GoogleChart

---
+++ UwAmp/www/Googlechart/base.php
@@ -0,0 +1,27 @@
+<?php
+$jsonString = '
+{                                          
+  "orderID": 12345,                        
+  "shopperName": "Ваня Иванов",             
+  "shopperEmail": "ivanov@example.com", 
+  "contents": [                            
+    {                                      
+      "productID": 34,                     
+      "productName": "Супер товар",        
+      "quantity": 1                       
+    },                                     
+    {                                      
+      "productID": 56,                     
+      "productName": "Чудо товар",       
+      "quantity": 3                        
+    }                                      
+  ],                                       
+  "orderCompleted": true                   
+}                                          
+';
+ 
+$cart = json_decode( $jsonString );
+var_dump($cart);
+echo $cart->shopperEmail ;
+echo $cart->contents[1]->productName;
+?>
