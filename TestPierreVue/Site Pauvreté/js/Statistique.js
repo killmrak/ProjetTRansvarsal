7bbcfcf8954f7b0ccf137a830fb1f6d16dfbab40
Pierre RUDELOU <pierre.rudelou@hotmail.fr>
2016-03-13 11:19:55 +0100

    Création du fichier Statistique.js

---
+++ TestPierreVue/Site Pauvreté/js/Statistique.js
@@ -0,0 +1,18 @@
+/*
+Fichier Javascript incluant toutes les fonctions du bloc statistique
+*/
+
+
+
+// Affiche le graphe en fonction du bouton pressé
+function afficherGraphe() {
+	document.getElementById("chart").innerHTML="<img src='Ressources/graphe.png'/>";
+
+}
+
+
+// Affiche le tableau en fonction du bouton pressé
+function afficherTableau(){
+	document.getElementById("tab").innerHTML="<img src='Ressources/Logo.png'/>";
+}
+
