a49936cb9bc1ccfcd34416d9cb6b41e314b2d7b4
Pierre RUDELOU <pierre.rudelou@hotmail.fr>
2016-03-13 14:29:01 +0100

    Télécharger le bon fichier txt
    Prochainement extraire données de la BDD

--- TestPierreVue/SitePauvrete/ExporterFichier.php
+++ TestPierreVue/SitePauvrete/ExporterFichier.php
@@ -1,7 +1,11 @@
  <?php
+$fileName  = fopen('Ressources/Statistiques/Ratio_de_la_population_pauvre_en_fonction_du_seuil_de_pauvrete_national_rural.txt',"w+");
+ftruncate($fileName ,0);
+fputs($fileName ,"J'aime les coooooooooooooooooooooooooooooookies et les bonbons!"); // on écrit le nom et email dans le fichier
+ 
  
 // Téléchargement d'un fichier txt 
-$fileName = "coucou.txt";
+$fileName="Ressources/Statistiques/Ratio_de_la_population_pauvre_en_fonction_du_seuil_de_pauvrete_national_rural.txt";
 header('Content-disposition: attachment; filename='.$fileName); 
 header('Content-Type: application/force-download'); 
 header("Content-Transfer-Encoding: binary"); 
